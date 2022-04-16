5efec7a6d8171059a89c4f0cbba6c364c279cd0e
Shreeyash Kulkarni <shreeyashkulkarni3@gmail.com>
2022-04-16 23:30:05 +0530

    added Solution.java

--- Solution.java
+++ Solution.java
@@ -3,7 +3,15 @@ class Solution
 	public static void main(String[] args)
 	{
 
+		int c=0,b=10,a = 20;
+
 		System.out.println("Shreeyash kulkarni Hello world!!!");
+
+		c = a * b;
+
+		System.out.println("C : ",c);
+
+
 
 	}
 	
