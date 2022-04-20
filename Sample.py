522c77f29022181d3b8f311213f52da4c72fd21a
Shreeyash Kulkarni <Shreeyashkulkarni3@gmail.com>
2022-04-20 23:10:24 +0530

    initial Maximum

---
+++ Sample.py
@@ -0,0 +1,23 @@
+a = 10
+b = 20
+
+
+
+c = max(a,b)
+print "max : ",c
+
+
+
+
+
+def Maximum(a,b):
+	if a > b:
+		print"a is Maximum, a =  ",a
+	else:
+		print"b is Maximum, b =  ",b
+
+Maximum(a,b)
+
+
+
+
