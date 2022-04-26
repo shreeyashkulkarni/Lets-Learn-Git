e0c27da7aa1923904b30b43dd0c77ff2edddbb56
Shreeyash Kulkarni <Shreeyashkulkarni3@gmail.com>
2022-04-26 13:14:19 +0530

    Updated Sample.py

--- Sample.py
+++ Sample.py
@@ -1,43 +1,49 @@
 
-#Finding Maximum
+# Finding Maximum
 
-a = 10
-b = 20
-
-
-
-c = max(a,b)
-print "max : ",c
+# a = 10
+# b = 20
 
 
 
-
-
-def Maximum(a,b):
-	if a > b:
-		print"a is Maximum, a =  ",a
-	else:
-		print"b is Maximum, b =  ",b
-
-Maximum(a,b)
-
-#Factorial 
-
-
-
-a =int(input("Enter a number"))
-c = 1
-
-for i in range(1,a+1):
-	c = c * i
-
-
-print"The Factorial is  ",c
+# c = max(a,b)
+# print "max : ",c
 
 
 
 
 
+# def Maximum(a,b):
+# 	if a > b:
+# 		print"a is Maximum, a =  ",a
+# 	else:
+# 		print"b is Maximum, b =  ",b
+
+# Maximum(a,b)
+
+# #Factorial 
 
 
+
+# a =int(input("Enter a number"))
+# c = 1
+
+# for i in range(1,a+1):
+# 	c = c * i
+
+
+# print"The Factorial is  ",c
+
+
+
+
+
+def Factorial(n):
+
+	return 1 if (n == 1  or n == 0) else n * Factorial(n -1)
+
+
+
+
+print(Factorial(5))
 
